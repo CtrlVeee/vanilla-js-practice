f0a5b5f697a99b1d4d64ebc7ccf81fb15de426e7
CtrlVeee <voightmansibang2@gmail.com>
2022-03-30 14:58:33 +0800

    work

--- note-app/script.js
+++ note-app/script.js
@@ -1,6 +1,15 @@
 const btn = document.querySelector('#input')
 const txt = document.querySelector('textarea')
+const temp = document.querySelector("template")
+const notes = document.querySelector('.note-list')
 
 btn.addEventListener('click', () => {
-    console.log(txt.value)
+    let div= document.createElement('div')
+    div.setAttribute("class", "note-item")
+
+    div.appendChild(temp.content.cloneNode(true))
+    
+    notes.appendChild(div)
+    console.log(notes)
+    
 })
