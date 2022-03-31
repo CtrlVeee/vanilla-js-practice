1129f0a835c76d97b9eb23738a1fc1cecf4e465e
CtrlVeee <voightmansibang2@gmail.com>
2022-03-31 14:53:49 +0800

    hmm

---
+++ Music App/script.js
@@ -0,0 +1,33 @@
+const tracklist = [
+    {
+        name: 'Jujutsu Kaisen Op 1',
+        src: "Music App\audio\廻廻奇譚 - Eve MV.mp3"
+    }, 
+    {
+        name: 'Mantis Lords',
+        src: "Music App\audio\Hollow Knight OST - Mantis Lords.mp3"
+    }, 
+    {
+        name: 'Fallen Donw (Cover)',
+        src: "Music App\audio\VGR, CG5 - Fallen Down (Undertale Music Video).mp3"
+    }, 
+    {
+        name: 'Maybe Daniels all the push',
+        src: "Music App\audio\Maybe Daniel's All the Push I Need.mp3"
+    }
+]
+
+let index = 0
+
+window.addEventListener("load", ()=>{
+    console.log("loaded")
+    const prev = document.querySelector(#prev)
+    const next = document.querySelector(#next)
+    const title = document.querySelector(h3)
+    loadMusic()
+    
+})
+
+function loadMusic(index) {
+
+}
