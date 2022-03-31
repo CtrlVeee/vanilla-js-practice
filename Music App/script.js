4c0b0d08b83b5256224fb79b0b1bbd5ea43d9711
CtrlVeee <voightmansibang2@gmail.com>
2022-03-31 17:01:46 +0800

    work

--- Music App/script.js
+++ Music App/script.js
@@ -1,33 +1,54 @@
 const tracklist = [
     {
         name: 'Jujutsu Kaisen Op 1',
-        src: "Music App\audio\廻廻奇譚 - Eve MV.mp3"
+        src: "audio\\廻廻奇譚 - Eve MV.mp3"
     }, 
     {
         name: 'Mantis Lords',
-        src: "Music App\audio\Hollow Knight OST - Mantis Lords.mp3"
+        src: "audio\\Hollow Knight OST - Mantis Lords.mp3"
     }, 
     {
         name: 'Fallen Donw (Cover)',
-        src: "Music App\audio\VGR, CG5 - Fallen Down (Undertale Music Video).mp3"
+        src: "audio\\VGR, CG5 - Fallen Down (Undertale Music Video).mp3"
     }, 
     {
         name: 'Maybe Daniels all the push',
-        src: "Music App\audio\Maybe Daniel's All the Push I Need.mp3"
+        src: "audio\\Maybe Daniel's All the Push I Need.mp3"
     }
 ]
+
+const prev = document.querySelector('#prev')
+const next = document.querySelector('#next')
+const title = document.querySelector('h3')
+const audio = document.querySelector('audio')
 
 let index = 0
 
 window.addEventListener("load", ()=>{
     console.log("loaded")
-    const prev = document.querySelector(#prev)
-    const next = document.querySelector(#next)
-    const title = document.querySelector(h3)
-    loadMusic()
+    loadMusic(index, title, audio)
     
 })
 
-function loadMusic(index, elem) {
+prev.addEventListener("click", ()=> {
+    index--;
+    if (index < 0) {
+        index = tracklist.length -1
+    }
+    loadMusic(index, title, audio)
+})
+
+next.addEventListener("click", ()=> {
+    index++;
+    if (index >= tracklist.length) {
+        index = 0
+    }
+    loadMusic(index, title, audio)
+})
+
+function loadMusic(index, elem, audio) {
     elem.textContent = tracklist[index]["name"]
+    audio.setAttribute('src', tracklist[index]["src"])
+    console.log(audio.getAttribute('src'))
+
 }
