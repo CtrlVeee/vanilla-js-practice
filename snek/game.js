bc9857715d331a7e177eae57a9147353e6fdc57d
CtrlVeee <voightmansibang2@gmail.com>
2022-04-12 16:31:21 +0800

    complete

--- snek/game.js
+++ snek/game.js
@@ -1,8 +1,18 @@
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
+import { update as updateFood, draw as drawFood} from './food.js'
+import { outsideGrid } from './grid.js'
+
 let lastRenderTime = 0
 const gameBoard = document.getElementById("game-board")
+let gameOver = false
 
 function main(currentTime) {
+    if (gameOver) {
+        if (confirm("you lost. Press ok to restart")) {
+            window.location ='./index.html'
+        }
+        return 
+    }
     window.requestAnimationFrame(main)
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
     if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
@@ -17,9 +27,16 @@ window.requestAnimationFrame(main)
 
 function update() {
     updateSnake()
+    updateFood(gameBoard)
+    checkDeath()
 }
 
 function draw() {
     gameBoard.innerHTML = '';
     drawSnake(gameBoard)
+    drawFood(gameBoard)
+}
+
+function checkDeath() {
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
