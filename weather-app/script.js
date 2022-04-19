3784f175515d115831778c4c4cffabefc768f967
CtrlVeee <voightmansibang2@gmail.com>
2022-04-19 10:14:11 +0800

    eh

---
+++ weather-app/script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector("input[type='submit']")
+const input = document.querySelector("input[type='text']")
+const place = document.getElementById("place")
+const temp = document.getElementById("temp")
+const desc = document.getElementById("desc")
+//console.log(btn.value)
+//console.log(input.value)
+//console.log(desc.textContent)
+
