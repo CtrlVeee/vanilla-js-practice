4f359dee464a366886d8237d229728c0b14e942b
CtrlVeee <voightmansibang2@gmail.com>
2022-04-19 17:20:21 +0800

    its all coming together

--- quiz-app/app.js
+++ quiz-app/app.js
@@ -2,28 +2,28 @@ const data = [
     {
         header: "Question 1",
         options: [
-            "option 1",
-            "option 2",
-            "option 3",
-            "option 4"
+            ['option 1', true],
+            ['option 2', false],
+            ['option 3', false],
+            ['option 4', false]
         ]
     },
     {
         header: "Question 2",
         options: [
-            "option 5",
-            "option 6",
-            "option 7",
-            "option 8"
+            ['option 5', false],
+            ['option 6', false],
+            ['option 7', true],
+            ['option 8', false]
         ]
     },
     {
         header: "Question 3",
         options: [
-            "option 9",
-            "option 10",
-            "option 11",
-            "option 12"
+            ['option 9', false],
+            ['option 10', true],
+            ['option 11', false],
+            ['option 12', false]
         ]
     }
 ]
@@ -32,23 +32,32 @@ var ops = document.getElementsByTagName('ul')[0]
 var enter = document.getElementById('enter')
 var start = document.getElementById('start')
 var next = document.getElementById('next')
+var head = document.getElementById('desc')
 
 start.addEventListener('click', () => {
     startApp()
 })
 
+let currentItem = 0
 next.addEventListener('click', () => {
-    nextItem()
+    currentItem ++
+    loadItem(currentItem)
 })
 
 function startApp() {
     ops.classList.remove('hide')
     start.classList.add('hide')
     start.textContent = "Restart";
+
+    loadItem(currentItem)
 }
 
-function nextItem() {
-    console.log(children)
+function loadItem(currentItem) {
+    children.forEach(child => {
+        let i = children.indexOf(child)
+        head.textContent = data[currentItem]['header']
+        child.textContent = data[currentItem]['options'][i][0]
+    })
 }
 
 children = [...ops.children]
@@ -58,8 +67,8 @@ children.forEach(child => {
             elem.classList.remove('selected')
         })
         e.target.classList.add('selected')
-        if (enter.classList.contains('hide')) {
-            enter.classList.remove('hide')
+        if (next.classList.contains('hide')) {
+            next.classList.remove('hide')
         }
     }
 )})
