66c3cbd53a612e8a95df0876c7edd74cb1094c3e
CtrlVeee <voightmansibang2@gmail.com>
2022-04-20 15:03:00 +0800

    complete

--- weather-app/script.js
+++ weather-app/script.js
@@ -4,6 +4,20 @@ const place = document.getElementById("place")
 const temp = document.getElementById("temp")
 const desc = document.getElementById("desc")
 //console.log(btn.value)
-//console.log(input.value)
+console.log(input.value)
 //console.log(desc.textContent)
+apiKey = '6c786c4b9ac5f28acb4bf55cce87d835'
 
+btn.addEventListener('click',function(){
+    cityName = input.value
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid='+apiKey)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        place.textContent = data.name
+        desc.textContent = data.weather[0].description
+        temp.textContent = Math.round(data.main.temp - 273.15) + " degrees"
+    })
+    .catch(err => alert("city name bruh"))
+
+})
